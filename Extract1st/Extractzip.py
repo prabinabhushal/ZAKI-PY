12c2a89dbcbd84bb0919ca40deb1914163e92c3c
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-09 18:04:27 +0545

    update

--- Extract1st/Extractzip.py
+++ Extract1st/Extractzip.py
@@ -3,24 +3,24 @@ import zipfile
 import os
 import json
 from decimal import Decimal
+from pathlib import Path
 
 
 def extract(zip_file):
 
+    zip_path = Path(zip_file)
+    output_path = zip_path.parent
     network = []
     provider = []
     
-    folder_path = '/home/prabina-bhushal/Desktop/PrabinaZaki/3ETL-prabinaBhushal/IgnoreFolder/MagnaCarePPO_In-Network11'
     
     def con_decimal(obj):
         if isinstance(obj, Decimal):
             return float(obj)
     
-    for file in os.listdir(folder_path):
-        if file.endswith('.zip'):
-            zip_path = os.path.join(folder_path, file)
+
     
-            with zipfile.ZipFile(zip_path, 'r') as z:
+    with zipfile.ZipFile(zip_file, 'r') as z:
                 for name in z.namelist():
                     if name.endswith('.json'):
                         with z.open(name) as f:
@@ -30,9 +30,12 @@ def extract(zip_file):
                         with z.open(name) as f:
                             for item in ijson.items(f, 'in_network.item'):
                                 network.append(item)
-    
-    net_path = '/home/prabina-bhushal/Desktop/PrabinaZaki/3ETL-prabinaBhushal/IgnoreFolder/in_network.json'
-    pro_path = '/home/prabina-bhushal/Desktop/PrabinaZaki/3ETL-prabinaBhushal/IgnoreFolder/provider_table.json'
+ 
+
+    net_path = output_path/ 'in_network.json'
+    pro_path = output_path/ 'provider_table.json'
+
+   
     with open(net_path, 'w') as f1:
         json.dump(network, f1, indent=4, default=con_decimal)
     
