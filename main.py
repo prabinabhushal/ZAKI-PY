f79318880baf42dbb07d01c46b32bf8a12289bdd
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-12 14:25:07 +0545

    optional argument

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import sys
+# import sys
+import argparse
 from Extract1st import Extractzip
 from ScrubExplode2nd import Scrubzip
 from LoadSQL import Loadzip
 
 def main ():
-    zip_file = sys.argv[1]
+    #  zip_file = sys.argv[1]
+ 
+    parser = argparse.ArgumentParser(description="ETL for ZIP file with network and provider data.") #argumentParse object
+    parser.add_argument("zip_file", help="Full path to the ZIP file to process")
+    args = parser.parse_args()
+   
 
-    net_path,pro_path= Extractzip.extract(zip_file)
+    net_path,pro_path= Extractzip.extract(args.zip_file)
     net_parquet,pro_parquet=Scrubzip.scrub(net_path,pro_path)
     Loadzip.load(net_parquet,pro_parquet)
 
