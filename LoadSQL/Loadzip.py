8ac2f9f0a9d48f270a1f2ee2207b0e9b4137d931
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-12 12:10:43 +0545

    update load

--- LoadSQL/Loadzip.py
+++ LoadSQL/Loadzip.py
@@ -25,21 +25,21 @@ def load(net_parquet,pro_parquet):
         password = "123",
     )
     cursor = conn.cursor()
-    provider_table = """
-    CREATE TABLE IF NOT EXISTS provider_table (
+    pprovider_table = """
+    CREATE TABLE IF NOT EXISTS pprovider_table (
         provider_group_id INT,
         npi INT,
         tin_type SMALLINT,
         tin TEXT 
     );
     """
-    cursor.execute(provider_table)
+    cursor.execute(pprovider_table)
     conn.commit()
     spark2 = spark.read.parquet(pro_parquet)
     spark2.printSchema()
-    spark2.write.jdbc(url=jdbc_url, table="provider_table", mode="append", properties= conn_properties)
+    spark2.write.jdbc(url=jdbc_url, table="pprovider_table", mode="append", properties= conn_properties)
     in_network_table = """
-    CREATE TABLE IF NOT EXISTS in_network (
+    CREATE TABLE IF NOT EXISTS in_network_table (
         billing_code TEXT,
         billing_code_type TEXT,
         negotiation_arrangement TEXT,
@@ -55,4 +55,4 @@ def load(net_parquet,pro_parquet):
     conn.commit()
     spark1 = spark.read.parquet(net_parquet)
     spark1.printSchema()
-    spark1.write.jdbc(url=jdbc_url, table="in_network", mode="append", properties=conn_properties)
+    spark1.write.jdbc(url=jdbc_url, table="in_network_table", mode="append", properties=conn_properties)
