8292225555a176e9af3cd6597fa8b50c0a39621f
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-12 12:09:38 +0545

    update Ex

--- Extract1st/Extractzip.py
+++ Extract1st/Extractzip.py
@@ -1,46 +1,41 @@
 import ijson
 import zipfile
-import os
 import json
 from decimal import Decimal
 from pathlib import Path
 
-
 def extract(zip_file):
+    script_dir = Path(__file__).resolve().parent
 
-    zip_path = Path(zip_file)
-    output_path = zip_file
+    output_dir = script_dir.parent / 'output'
+
+    output_dir.mkdir(exist_ok=True)
+
     network = []
     provider = []
-    
-    
+
     def con_decimal(obj):
         if isinstance(obj, Decimal):
             return float(obj)
-    
 
-    
     with zipfile.ZipFile(zip_file, 'r') as z:
-                for name in z.namelist():
-                    if name.endswith('.json'):
-                        with z.open(name) as f:
-                            for item in ijson.items(f, 'provider_references.item'):
-                                provider.append(item)
-    
-                        with z.open(name) as f:
-                            for item in ijson.items(f, 'in_network.item'):
-                                network.append(item)
- 
+        for name in z.namelist():
+            if name.endswith('.json'):
+                with z.open(name) as f:
+                    for item in ijson.items(f, 'provider_references.item'):
+                        provider.append(item)
 
-    net_path = output_path/ 'in_network.json'
-    pro_path = output_path/ 'provider_table.json'
+                with z.open(name) as f:
+                    for item in ijson.items(f, 'in_network.item'):
+                        network.append(item)
 
-   
+    net_path = output_dir / 'in_network.json'
+    pro_path = output_dir / 'provider_table.json'
+
     with open(net_path, 'w') as f1:
         json.dump(network, f1, indent=4, default=con_decimal)
-    
+
     with open(pro_path, 'w') as f2:
         json.dump(provider, f2, indent=4, default=con_decimal)
 
-    return net_path,pro_path
-    
+    return net_path, pro_path
