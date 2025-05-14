7d39e69b4fb3781a19a038e5fe839703ca2b37d8
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-14 15:14:35 +0545

    mainn file

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 # import sys
-import argparse
-from Extract1st import Extractzip
-from ScrubExplode2nd import Scrubzip
-from LoadSQL import Loadzip
-
+from ETL import ExtractTransferLoad
 def main ():
     #  zip_file = sys.argv[1]
  
-    parser = argparse.ArgumentParser(description="ETL for ZIP file with network and provider data.") #argumentParse object
-    parser.add_argument("--zip_file",required=True, help="Full path to the ZIP file to process")  #optional argument
-    args = parser.parse_args()
-   
-
-    net_path,pro_path= Extractzip.extract(args.zip_file)
-    net_parquet,pro_parquet=Scrubzip.scrub(net_path,pro_path)
-    Loadzip.load(net_parquet,pro_parquet)
-
+    etl = ExtractTransferLoad()
+    etl.run()
 
 if __name__ == "__main__":
     main()
