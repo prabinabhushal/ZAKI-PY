3e733dcc8ef63010c82d9ba129dd549939e453e8
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-20 15:31:30 +0545

    minor changes of new provider

--- Extract1st/Extract_nrpr.py
+++ Extract1st/Extract_nrpr.py
@@ -6,16 +6,18 @@ from pathlib import Path
 from io import BytesIO
 import gzip
 
-def merge(zip_file):
+def merge(zip_file,pdetail_file):
     output_dir = Path('output_nrpr')
     output_dir.mkdir(exist_ok=True)
 
     nrpr_path = output_dir / 'combined_nrpr.json'
+    pro_path= output_dir/'add_provider'
+
     def con_decimal(obj):
         if isinstance(obj, Decimal):
             return float(obj)
 
-    with open(nrpr_path, 'w') as nrpr_file:
+    with open(nrpr_path,'w') as nrpr_file:
             with zipfile.ZipFile(zip_file, 'r') as outer_zip:
                 for name in outer_zip.namelist():
                     if name.endswith('.json.gz'):
@@ -26,5 +28,12 @@ def merge(zip_file):
                                     item['source'] = 'in_network'
                                     nrpr_file.write(json.dumps(item, default=con_decimal) + '\n')
 
+    with zipfile.ZipFile(pdetail_file, 'r') as z:
+        for name in z.namelist():
+            if name.endswith('.parquet'):
+                with z.open(name, 'r') as f:
+                    with open(pro_path, 'wb') as f1:
+                        f1.write(f.read())
 
-    return str(nrpr_path)
+
+    return nrpr_path,pro_path
