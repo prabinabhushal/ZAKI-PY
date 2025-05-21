21094a347879cf56510d5f81d6fc638ea62af27b
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-21 16:17:37 +0545

    add nrpr

--- Extract1st/Extract_nrpr.py
+++ Extract1st/Extract_nrpr.py
@@ -1,39 +1,19 @@
-import ijson
-import zipfile
+#Extracting the folder of compressed file(.gz)
 import json
-from decimal import Decimal
-from pathlib import Path
-from io import BytesIO
 import gzip
+from pathlib import Path
 
-def merge(zip_file,pdetail_file):
+def merge(folder):
     output_dir = Path('output_nrpr')
     output_dir.mkdir(exist_ok=True)
 
     nrpr_path = output_dir / 'combined_nrpr.json'
-    pro_path= output_dir/'add_provider'
+    gz_folder = Path(folder)  
 
-    def con_decimal(obj):
-        if isinstance(obj, Decimal):
-            return float(obj)
-
-    with open(nrpr_path,'w') as nrpr_file:
-            with zipfile.ZipFile(zip_file, 'r') as outer_zip:
-                for name in outer_zip.namelist():
-                    if name.endswith('.json.gz'):
-                        with outer_zip.open(name) as zipped_file:
-                            data = zipped_file.read()
-                            with gzip.open(BytesIO(data), 'rt', encoding='utf-8') as f:
-                                for item in ijson.items(f, 'in_network.item'):
-                                    item['source'] = 'in_network'
-                                    nrpr_file.write(json.dumps(item, default=con_decimal) + '\n')
-
-    with zipfile.ZipFile(pdetail_file, 'r') as z:
-        for name in z.namelist():
-            if name.endswith('.parquet'):
-                with z.open(name, 'r') as f:
-                    with open(pro_path, 'wb') as f1:
-                        f1.write(f.read())
-
-
-    return nrpr_path,pro_path
+    with open(nrpr_path, 'w', encoding='utf-8') as nrpr_file:
+        for file in gz_folder.glob('*.json.gz'):
+            with gzip.open(file, 'rt', encoding='utf-8') as f:
+                data = json.load(f)
+                json.dump(data, nrpr_file)
+                nrpr_file.write('\n')  
+    return nrpr_path
