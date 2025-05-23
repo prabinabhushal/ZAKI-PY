93705cac91cf447c7d85db68bf8b80a84c8ca8fe
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-23 16:34:05 +0545

    update

--- ETL_nrpr.py
+++ ETL_nrpr.py
@@ -24,20 +24,18 @@ class ExtractTransferLoad:
         self.password= f1['POSTGRES']['password']
 
     def run(self,args,logger):
-        logger.info("Initializing Spark session")
-        
         logger.info("Initializing ETL process")
 
         logger.info("Starting extraction of nrpr gz file")
         nrpr_path= Extract_nrpr.merge(args.folder)
 
-        
+        logger.info("Initializing Spark session")
         self.spark = SparkSession.builder.appName('ETL').config("spark.driver.memory", self.container4).getOrCreate()
 
         logger.info("Starting scrub of nrpr step")
         pr,provider_detail1,network_nr,df3,df2= Scrub_nrpr.scrub(nrpr_path, args.pd,self)
         logger.info("Starting process of nrpr step")
-        nr_parquet,pr_parquet=process_nrpr.process(pr,provider_detail1,network_nr,df3,self)
+        nr_parquet,pr_parquet=process_nrpr.process(pr,provider_detail1,network_nr,df3,logger)
 
         logger.info("Starting load the data of nrpr step")
         Load_nrpr.load(nr_parquet,pr_parquet,df2,self)
