cebdb69347aa14a7f052a45129f2ef7378a23dda
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-23 16:36:43 +0545

    update

--- LoadSQL/Load_nrpr.py
+++ LoadSQL/Load_nrpr.py
@@ -7,7 +7,6 @@ def load(nr_parquet,pr_parquet,df2,etl):
     dbname = etl.dbname
     user = etl.user
     password= etl.password
-
     
     jdbc_url = f"jdbc:postgresql://{host}:{port}/{dbname}"
     conn_properties = {
@@ -25,7 +24,7 @@ def load(nr_parquet,pr_parquet,df2,etl):
     cursor = conn.cursor()
     cursor.execute("DROP TABLE IF EXISTS pr_table;")
     cursor.execute("DROP TABLE IF EXISTS nr_table;")
-    cursor.execute("DROP TABLE IF EXISTS bill_table;")
+    cursor.execute("DROP TABLE IF EXISTS taxonomy.billing_taxonomy;")
 
     # CREATE EXTENSION postgis;
 
@@ -34,19 +33,19 @@ def load(nr_parquet,pr_parquet,df2,etl):
         provider_group_id BIGINT,                
         npi BIGINT,                          
         tin_type SMALLINT,                    
-        tin VARCHAR(12),                           
-        prv_city VARCHAR(255),                 
-        prv_phone VARCHAR(12),                   
-        prv_state CHAR(2),                   
-        prv_street_1 VARCHAR(255),            
-        prv_zip VARCHAR(10),                      
+        tin BIGINT,                           
+        prv_city VARCHAR,                 
+        prv_phone VARCHAR,                   
+        prv_state VARCHAR,                   
+        prv_street_1 VARCHAR,            
+        prv_zip VARCHAR,                      
         latitude DOUBLE PRECISION,              
         longitude DOUBLE PRECISION,            
         prv_type_code INT,           
-        provider_full_name VARCHAR(255),      
+        provider_full_name VARCHAR,      
         all_specialties TEXT[],                
         taxonomy_codes TEXT[],
-        geos GEOGRAPHY(Point, 4326)                   
+        geom GEOGRAPHY(Point, 4326) GENERATED ALWAYS AS (ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)::geography)STORED                
     );
     """
     cursor.execute(pr_table)
@@ -57,35 +56,39 @@ def load(nr_parquet,pr_parquet,df2,etl):
     spark2.write.jdbc(url=jdbc_url, table="pr_table", mode="append", properties= conn_properties)
     nr_table = """
     CREATE TABLE nr_table (
-        billing_code VARCHAR(5),
-        billing_code_type VARCHAR(10),
-        negotiation_arrangement VARCHAR(10),
+        billing_code VARCHAR,
+        billing_code_type VARCHAR,
+        negotiation_arrangement VARCHAR,
         provider_group_id BIGINT,
-        negotiated_type VARCHAR(12),
+        negotiated_type VARCHAR,
         negotiated_rate DOUBLE PRECISION,
-        billing_class VARCHAR(15),
+        billing_class VARCHAR,
         billing_code_modifier TEXT[],
         service_code INTEGER[],
-        taxonomy_list VARCHAR,
-        geos GEOGRAPHY(Point, 4326)   
+        taxonomy_list TEXT[]
+         
     );
     """
     cursor.execute(nr_table)
     conn.commit()
+
     spark1 = spark.read.parquet(nr_parquet)
     spark1.show(5)
     spark1.printSchema()
     spark1.write.jdbc(url=jdbc_url, table="nr_table", mode="append", properties=conn_properties)
-
-    bill_table = """
-    CREATE TABLE bill_table (
+ 
+    cursor.execute("CREATE SCHEMA IF NOT EXISTS taxonomy;")
+    conn.commit()
+    create_table_query = """
+    DROP TABLE IF EXISTS taxonomy.billing_taxonomy;
+    CREATE TABLE IF NOT EXISTS taxonomy.billing_taxonomy (
         billing_code VARCHAR(5),
         billing_code_type VARCHAR,
-        billing_description VARCHAR,
-        taxonomy_list VARCHAR,
-        geos GEOGRAPHY(Point, 4326) 
+        billing_description TEXT,
+        taxonomy_list TEXT[]
     );
     """
-    cursor.execute(bill_table)
+    cursor.execute(create_table_query)
     conn.commit()
-    df2.write.jdbc(url=jdbc_url, table="bill_table", mode="append", properties=conn_properties)
+    df2.write.jdbc(url=jdbc_url,table="taxonomy.billing_taxonomy",mode="append", properties=conn_properties)
+    cursor.close()
