0a7d3be2eb72ef4d69aba1a9d722e4dbfd6ff9f2
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-05-26 16:47:11 +0545

    update

--- Process/process_nrpr.py
+++ Process/process_nrpr.py
@@ -6,8 +6,6 @@ def process(pr,provider_detail1,network_nr,df3,logger):
     nrpr_pr = pr.join(provider_detail1,on =["npi","tin"],how= "inner")
     left_npi = pr.join(provider_detail1,on ="npi",how= "left_anti")
 
-    nrpr_pr.show(5)
-
     #Join network table
     nrpr_network = network_nr.join(df3, on="billing_code", how= 'inner')
     nrpr_network.printSchema()
@@ -17,8 +15,7 @@ def process(pr,provider_detail1,network_nr,df3,logger):
     logger.info("After joining nr and pr, nrpr_final is filtered to keep only rows with matching taxonomy codes.")
     nrpr_final = nrpr_pr.join(nrpr_network ,on ="provider_group_id",how = "inner" )
     specialized_filter = nrpr_final.filter(size(array_intersect(col("taxonomy_codes"), col("taxonomy_list"))) > 0)
-    specialized_filter.show(5)
-    
+
     nr_parquet ='output_nrpr/nr'
     pr_parquet ='output_nrpr/pr'
     left__nrpr_npi_parquet ='output_nrpr/left_npi'
