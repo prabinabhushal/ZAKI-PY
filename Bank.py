7f582eec2fb0a18d56be289f0b6a13a7afc1a4b8
Prabina-Bhushal <bhushalprabina@gmail.com>
2025-06-18 14:18:59 +0545

    updated

---
+++ Bank.py
@@ -0,0 +1,159 @@
+from abc import ABC, abstractmethod
+import json
+
+class Bank(ABC):
+    bank_name = 'Nabil Bank'
+
+
+    def __init__(self, name, pin, address, balance=0,status ='Active'):
+        self.id = id(self)
+        self.name = name
+        self.__pin = pin
+        self.address = address
+        self.balance = balance
+        self.status = status
+
+    @abstractmethod
+    def deposite(self, amount):
+        pass
+
+    @abstractmethod
+    def withdrawal(self,amount):
+        pass
+
+    @abstractmethod
+    def transfer(self,amount,receiver):
+        pass
+
+    def check_data_type(data_type):
+        def check_valid(func):
+            def wrapper(*args):
+                if not isinstance (args[1],data_type):
+                    print('Invalid DataType')
+                    return False
+                return func(*args)
+            return wrapper
+        return check_valid
+    
+        
+    def check_validation(func):
+        def wrapper(self,amount,*args):
+            if self.balance < amount:
+                print(f"Your balance is insufficient {self.balance}" )
+                return False
+            return func(self,amount,*args)
+        return wrapper
+  
+        
+class Staff(Bank):
+    def __init__(self, name, pin, address, balance):
+        super().__init__(name, pin, address, balance)
+        self.break_time = ['12:00 pm' , '[1:00 pm]']
+        print(f"{self.name} is an employee based on {self.address} branch and her scheduled break time is {self.break_time}")
+
+    def deposite(self, amount):
+        if amount < 0:
+            print(f"Amount can't be negative {self.amount}")
+            return False
+        self.balance += amount
+        print(f"Your balance is {self.balance}")
+    
+
+    @Bank.check_data_type(int)
+    @Bank.check_validation
+    def withdrawal(self,amount):
+        print(f"{self.name} your {amount} has been withdrawn")
+        self.balance -= amount
+        print(f"Your balance is {self.balance}")
+
+      
+    @Bank.check_data_type(int)
+    @Bank.check_validation  
+    def transfer(self, amount, receiver_name):
+        print(f"{self.name} has transfer amount {amount} to {receiver_name}")
+        self.balance -= amount
+        receiver.balance += amount
+        print(f"Your balance is {self.balance}")
+        print(f"{receiver_name} balance is {receiver.balance}")
+
+    @property
+    def get_pin(self):
+        return self.__pin
+
+    @get_pin.setter
+    def pin(self, new_pin):
+        self.__pin = new_pin
+    print("Pin is updated.")
+
+class Customer(Bank):
+    def __init__(self, name, pin, address, balance=0):
+        super().__init__(name, pin, address, balance)
+
+
+    def deposite(self, amount):
+        if amount < 0:
+            print(f"Amount can't be negative {self.amount}")
+            return False
+        self.balance += amount
+        print(f"Your balance is {self.balance}")
+    
+    @Bank.check_data_type(int)
+    @Bank.check_validation
+    def withdrawal(self,amount):
+        print(f"{self.name} your {amount} has been withdrawn")
+        self.balance -= amount
+        print(f"Your balance is {self.balance}")
+
+    @Bank.check_data_type(int)
+    @Bank.check_validation  
+    def transfer(self, amount, receiver_name):
+        print(f"{self.name} has transfer amount {amount} to {receiver_name}")
+        self.balance -= amount
+        receiver.balance += amount
+        print(f"Your balance is {self.balance}")
+
+    @property
+    def get_pin(self):
+        return self.__pin
+
+    @get_pin.setter
+    def pin(self, new_pin):
+        self.__pin = new_pin
+    print("Pin is updated.")
+
+
+staff = Staff('Prabina',123,'Koteshwor', 100)
+receiver = Staff('Namu',123,'Kalanki', 100)
+print(staff.id)
+print(staff.status)
+staff.deposite(1000)
+staff.withdrawal(500)
+staff.transfer(100,receiver.name)
+
+customer = Customer('Siri',123,'Koteshwor',1000)
+print(customer.id)
+print(staff.status)
+customer.deposite(1000)
+customer.withdrawal(500)
+customer.transfer(100,receiver.name)
+customer.pin = 4567
+print("New pin", customer.get_pin) 
+
+
+
+data = {
+    staff.name:
+    {'id': staff.id,'name': staff.name,'pin': staff._Bank__pin, 'address': staff.address,'balance': staff.balance}
+}
+with open('staff_data.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+
+data1 = {
+    customer.name:
+        {'id': customer.id,'name': customer.name,'pin': customer._Bank__pin, 'address': customer.address,'balance': customer.balance }
+}
+with open('customer_data.json', 'w') as f1:
+    json.dump(data, f1, indent=4)
+
+
